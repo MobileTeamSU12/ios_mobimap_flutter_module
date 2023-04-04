63eb449fa98f520a56292636d74e1e6d9f455e32
ThanhTC21 <thanhtc21@fpt.com.vn>
2023-04-04 10:17:32 +0700

    init

---
+++ mobimap_plugin/mobimap_plugin.framework/Headers/mobimap_plugin-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CusUIImagePlugin.h"
+#import "FMapHelperPlugin.h"
+#import "OpenUDIDPlugin.h"
+
+FOUNDATION_EXPORT double mobimap_pluginVersionNumber;
+FOUNDATION_EXPORT const unsigned char mobimap_pluginVersionString[];
+
