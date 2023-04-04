d1d286f9aece8735924c951039a15e5e92a63364
ThanhTC21 <thanhtc21@fpt.com.vn>
2023-04-04 12:02:01 +0700

    remove

--- Example/Pods/Target Support Files/Pods-ios_mobimap_module_flutter_Example/Pods-ios_mobimap_module_flutter_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ios_mobimap_module_flutter_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ios_mobimap_module_flutter_Example
-@end
--- Example/Pods/Target Support Files/ios_mobimap_module_flutter/ios_mobimap_module_flutter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ios_mobimap_module_flutter : NSObject
-@end
-@implementation PodsDummy_ios_mobimap_module_flutter
-@end
